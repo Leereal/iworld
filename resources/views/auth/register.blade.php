ba3fc1c61c64d5eae6824c0f56a83257e248b986
Leereal <leereal08@ymail.com>
2021-08-12 07:52:51 +0200

    Add Or in Ref

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
-                    <input type="hidden" name="ref" value="{{$ref}}">
+                    <input type="hidden" name="ref" value="{{$ref ?? ''}}">
                         @csrf
 
                         <div class="form-group row">
